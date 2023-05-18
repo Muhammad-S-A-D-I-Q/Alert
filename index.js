38edac780a0936e221432324b1d8e70abb4452f3
Muhammadsadiq2611 <115625441+Muhammadsadiq2611@users.noreply.github.com>
2023-05-19 00:30:51 +0500

    alert

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+document.write("using alert first time");
+
+
+var message0;
+var message0= "Hello world!"
+alert(message0);
+
+var message1;
+var message1 = " Nice you see you ";
+alert(message1);
+
+var names = " I am Muhammad Sadiq . ";
+alert(names);
+
+var edu = " Currently doing HSC... ";
+alert(edu);
+
+var beco = " Passion to become Web Developer ";
+alert(beco);
+
+//var design = " “ஜ۩۞۩ஜ_________ஜ۩۞۩ஜ________ஜ۩۞۩ஜ” ";
+//alert(design);
+
+
+
